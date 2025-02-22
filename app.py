18fadb1db438702684f238e3fc5c39d9110d6dca
Tapan Behera <107788391+Tapan1111@users.noreply.github.com>
2025-02-22 21:01:46 +0000

    added the job list dynamic from database.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from database import get_jobs_from_db
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  jobs = get_jobs_from_db()
+  return render_template('home.html', jobs=jobs)
 
 
 # from flask import render_template
--- database.py
+++ database.py
@@ -4,6 +4,10 @@ engine = create_engine(
     "mysql+pymysql://upkwmpqrrkcolnu0:myNoz7MsaMnnAHIUh82G@bfvcdyjv9kinltyhh9co-mysql.services.clever-cloud.com/bfvcdyjv9kinltyhh9co"
 )
 
-with engine.connect() as conn:
-    result = conn.execute(text("select * from bfvcdyjv9kinltyhh9co.jobs"))
-    print(result.all())
+def get_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs_list = []
+    for job in result.all():
+      jobs_list.append(job._asdict())
+    return jobs_list
