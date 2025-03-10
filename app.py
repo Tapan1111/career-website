123bb948e8a58fa04334253388a207d4674b94ef
Tapan1111 <elevenshelby003@gmail.com>
2025-03-11 01:47:01 +0530

    latest code changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from database import get_jobs_from_db
+from flask import Flask, jsonify, render_template, request
+from database import get_job_by_id, get_jobs_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -9,6 +9,19 @@ def hello_world():
   jobs = get_jobs_from_db()
   return render_template('home.html', jobs=jobs)
 
+@app.route("/jobs/<id>")
+def show_jobs(id):
+  job = get_job_by_id(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job=job)
+
+@app.route("/jobs/<id>/apply", methods=['post']) 
+def apply_to_jobs(id):
+  data = request.form
+  job = get_job_by_id(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html', application=data, job=job)
 
 # from flask import render_template
 
--- database.py
+++ database.py
@@ -11,3 +11,49 @@ def get_jobs_from_db():
     for job in result.all():
       jobs_list.append(job._asdict())
     return jobs_list
+
+def get_job_by_id(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("""
+                SELECT * FROM jobs 
+                WHERE id = :job_id
+            """),
+            {"job_id": id}
+        )
+        job = result.fetchone()
+        return job._asdict() if job else None
+    
+def add_application_to_db(job_id, application):
+    with engine.connect() as conn:
+        # SQL query with parameter binding
+        query = text("""
+            INSERT INTO applications (
+                job_id,
+                full_name,
+                email, 
+                linkedin_url,
+                resume_url
+            ) VALUES (
+                :job_id,
+                :full_name,
+                :email,
+                :linkedin_url,
+                :resume_url
+            )
+        """)
+        
+        # Execute with parameters
+        conn.execute(query, {
+            'job_id': job_id,
+            'full_name': application['name'],
+            'email': application['email'],
+            'linkedin_url': application.get('linkedin', ''),
+            'resume_url': application.get('resume', '')
+        })
+        
+        # Commit transaction
+        conn.commit()
+      
+
+      
